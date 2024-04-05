d1d04cab7be8f2ad22dbabe4207c83338688a27b
zippyh <zippyh@icloud.com>
2024-04-05 13:22:07 -0400

    created buy method

--- qBay.java
+++ qBay.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class qBay{
+    static String[][] items = {{"T-shirt", "clothes", "John", "Stan", "John.Stan@quinnipiac.edu", "$12.00"}, 
+    {"name of item", "category", "first name", "last name", "email", "price"}, 
+    {"name of item", "category", "first name", "last name", "email", "price"}, 
+    {"name of item", "category", "first name", "last name", "email", "price"}, 
+    {"name of item", "category", "first name", "last name", "email", "price"}};
     public static void main(String[] args) 
     {
-        
+        buy();
+    }
+
+    public static void buy() {
+        Scanner scan = new Scanner(System.in);
+        String userInput;
+        for(int i=0; i<items.length; i++) {
+            System.out.println(items[i][0]);
+        }
+        userInput = scan.nextLine();
+        for(int i=0; i<items.length; i++) {
+            if(userInput.equals(items[i][0])) {
+                for(int j=0; j<items[i].length-1; j++) {
+                    System.out.println(items[i][j]);
+                }
+            }
+        }
     }
 }
